22e48830b8d1a2f1a4a764a9a8a656e824949324
Renan Ribeiro <63807288+renanribs@users.noreply.github.com>
2020-05-29 14:24:37 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -8,12 +8,11 @@ lance_do_gui = Lance(gui, 150.0)
 
 leilao = Leilao('Celular')
 
-leilao.lances.append(lance_do_gui)
 leilao.lances.append(lance_do_yuri)
+leilao.lances.append(lance_do_gui)
 
 for lance in leilao.lances:
     print(f'O usuario {lance.usuario.nome} deu um lance de {lance.valor}')
-
 
 avaliador = Avaliador()
 avaliador.avalia(leilao)
